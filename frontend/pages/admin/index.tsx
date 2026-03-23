f4dcfc2ce972c904d8bcc48585740c30e8fdfe23
Papasani Manohar <161720298+TechNaom@users.noreply.github.com>
2026-03-23 23:48:50 +0530

    Update index.tsx

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import dynamic from 'next/dynamic'
 
 const TiptapEditor = dynamic(() => import('../../components/admin/TiptapEditor'), { ssr: false })
